c3155349610fd3ad12f74c423f58a0b386161259
Swapnil Sachan <swapnil.sachan2003@gmail.com>
2025-01-28 12:30:36 +0530

    init: next app setup

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Swapnil Sachan",
+  description: "Swapnil is a full-stack developer and a final year university student pursuing a CS degree. He has working experience with JS / TS and is currently learning to wirte APIs in Go.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
